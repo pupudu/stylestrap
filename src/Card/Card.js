b8108c4e737e8c24330aaff5dc975bce8e72df9f
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-11-04 00:48:43 +1100

    feat: Rewrite Card based on bootstrap base and flavors helpers

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
-import { withBrand } from '../core/ruleEngine';
+import { withStyles, getStylesByFlavor } from '../core/ruleEngine';
 
-const CardBody = styled.div`
-  background: transparent;
-  border-radius: 0.25rem;
-  outline: 0;
-  border: 0.0625rem solid transparent;
-  padding: 1rem;
-`;
+const CardBody = withStyles(['CardBody', 'card-body'], props => {
+  return {
+    padding: props.padding
+  };
+})();
 
-export const CardHeading = styled.div`
-  padding: 0.75rem 1.25rem;
-  margin-bottom: 0;
-  background-color: rgba(0, 0, 0, 0.03);
-  border-bottom: 1px solid rgba(0, 0, 0, 0.125);
-`;
+const CardHeader = withStyles(['CardHeader', 'card-header'])();
 
-const CardContainer = withBrand('Card', false)(styled.div`
-  background: #fff;
-  width: 25%;
-  margin-top: 1.25rem;
-  margin-bottom: 1.25rem;
-  border-radius: 0.25rem;
-  outline: 0;
-  border: 0.0625rem solid transparent;
-`);
+const CardContainer = withStyles(['Card', 'card'], (props, theme) => {
+  return getStylesByFlavor(props, theme, false);
+})();
 
 const Card = ({ children, ...rest }) => {
   return (
@@ -40,4 +26,4 @@ const Card = ({ children, ...rest }) => {
   );
 };
 
-export default Card;
+export { Card, CardHeader, CardBody };
