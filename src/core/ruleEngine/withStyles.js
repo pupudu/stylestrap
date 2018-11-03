1ff9d8d9c5a158f71f3d096ffd062ab5377889b4
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-11-03 17:09:07 +1100

    feat: Support for css props and custom classNames

--- src/core/ruleEngine/withStyles.js
+++ src/core/ruleEngine/withStyles.js
@@ -2,84 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 import { getStyleString } from './engine';
 
-export const withStyles = (
-  displayName,
-  getDefaultProps = () => ({}),
-  options = {}
-) => (Component = styled.div``) => {
+export const withStyles = (args, getStyleProps = () => ({})) => (
+  Component = styled.div``,
+  suffix
+) => {
+  if (!Array.isArray(args)) {
+    args = [args];
+  }
+  const [displayName, className] = args;
+
+  const getStylePropsWithCss = props => {
+    return {
+      ...getStyleProps(props, props.theme), // TODO merge with default theme as required
+      ...props.css // Enable passing a function as css
+    };
+  };
   const StyledComponent = styled(Component)`
-    ${props => getStyleString(props, getDefaultProps)};
+    ${props => getStyleString(props, getStylePropsWithCss)};
   `;
 
   const Wrapped = props => {
     return (
       <React.Fragment>
-        {options.prefix}
-        <StyledComponent {...props} />
-        {options.suffix}
+        <StyledComponent {...props} className={className} />
+        {suffix}
       </React.Fragment>
     );
   };
   Wrapped.displayName = displayName;
 
   return Wrapped;
-};
-
-export const withBrand = (
-  displayName,
-  effects = true,
-  ...rest
-) => Component => {
-  return withStyles(
-    displayName,
-    props => {
-      const brand = props.shade;
-      if (props.type === 'ghost') {
-        return {
-          color: {
-            base: brand,
-            hover: effects && 'white',
-            active: effects && 'white'
-          },
-          background: {
-            base: 'rgba(0,0,0,0)',
-            hover: effects && brand,
-            active:
-              effects && props.theme.colors.active(props.theme.colors[brand])
-          },
-          borderColor: brand
-        };
-      }
-      if (props.type == 'accent') {
-        return {
-          border: {
-            top: '1px solid #AAA',
-            right: '1px solid #AAA',
-            bottom: '1px solid #AAA',
-            left: `3px solid ${props.theme.colors[brand]}`
-          },
-          color: {
-            base: brand,
-            hover: effects && 'white',
-            active: effects && 'white'
-          },
-          background: {
-            base: 'rgba(0,0,0,0)',
-            hover: effects && brand,
-            active:
-              effects && props.theme.colors.active(props.theme.colors[brand])
-          }
-        };
-      }
-      return {
-        color: 'white',
-        background: {
-          base: brand,
-          hover: effects && 'auto',
-          active: effects && 'auto'
-        }
-      };
-    },
-    ...rest
-  )(Component);
 };
