e7f5edfedd615b108bfc1bb112f00dca3dd58968
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-11-04 17:54:38 +1100

    feat: Merge CSS grid components into one components

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,13 +1,28 @@
-import styled from 'styled-components';
 import { withStyles } from '../core/ruleEngine/withStyles';
 
-export const Grid = withStyles('Grid', props => ({
+const Grid = withStyles('Grid', props => ({
   padding: props.padding,
   margin: props.margin,
   gridTemplateColumns: props.columns,
   gridTemplateRows: props.rows,
-  gridGap: props.gap
-}))(styled.div`
-  display: grid;
-  position: relative;
-`);
+  gridGap: props.gap,
+  display: 'grid',
+  position: 'relative'
+}))();
+
+const Cell = withStyles('Cell', props => {
+  const [gridRowStart, gridRowEnd] =
+    typeof props.row === 'number' ? [props.row, props.row + 1] : props.row;
+  const [gridColumnStart, gridColumnEnd] =
+    typeof props.col === 'number' ? [props.col, props.col + 1] : props.col;
+  return {
+    alignSelf: props.align,
+    position: props.position,
+    gridRowStart,
+    gridRowEnd,
+    gridColumnStart,
+    gridColumnEnd
+  };
+})();
+
+export { Grid, Cell };
