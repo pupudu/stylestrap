ed0e216ff336555ccedd281f46197bea67203fec
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-11-05 01:10:21 +1100

    feat: Create directory for Form components

---
+++ src/Forms/FormGroup.js
@@ -0,0 +1,5 @@
+import { withStyles } from '../core/ruleEngine';
+
+const FormGroup = withStyles(['FormGroup', 'form-group'])();
+
+export { FormGroup };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 export { Button } from './Button';
 export { Grid, Cell } from './Grid';
 export { Container, Col, Row } from './FlexBox';
+export { FormGroup } from './Forms';
 
 // Presets
 export { Header } from './Header';
@@ -9,3 +10,6 @@ export { Header } from './Header';
 // Core modules and optionals
 export { default as defaultTheme } from './Themes/stylestrap';
 export { default as hipagesTheme } from './Themes/hipages';
+
+// Helpers
+export { withStyles } from './core/ruleEngine/withStyles';
