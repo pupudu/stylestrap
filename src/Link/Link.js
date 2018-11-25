35b58fbf5ad9580044023f2b6fc6c1327997c265
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2018-11-25 19:28:13 +1100

    feat: Implement Anchor and Link components

---
+++ src/Link/Link.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { withStyles } from '../core/ruleEngine';
+
+const Anchor = withStyles('Anchor', props => ({
+  color: props.color,
+  backgroundColor: 'transparent',
+  textDecoration: {
+    hover: 'underline'
+  }
+}))('a');
+
+Anchor.defaultProps = {
+  href: '#',
+  color: 'primary'
+};
+
+const Link = props => <Anchor {...props} className="btn" />;
+
+export { Link, Anchor };
