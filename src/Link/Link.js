55ff1362ea83d2f6f0a2e2e9cc178d44e9bd20d6
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-05-30 19:01:33 +1000

    feat: Enhance Anchor component

--- src/Link/Link.js
+++ src/Link/Link.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { withStyles } from '../core/ruleEngine';
 
-const Anchor = withStyles('Anchor', props => ({
-  color: props.color,
-  backgroundColor: 'transparent',
-  textDecoration: {
-    '&:hover': 'underline'
-  }
-}))('a');
+const getOnClickAnchor = onClick => e => {
+  e.preventDefault();
+  onClick(e);
+  return false;
+};
+
+const Anchor = withStyles(
+  'Anchor',
+  props => ({
+    color: props.color,
+    backgroundColor: 'transparent',
+    textDecoration: {
+      '&:hover': 'underline'
+    }
+  }),
+  ({ onClick }) => ({
+    onClick: getOnClickAnchor(onClick)
+  })
+)('a');
 
 Anchor.defaultProps = {
   href: '#',
