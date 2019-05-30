47e653aa897f16247c4b757f7a5fb6e1792a433e
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-05-30 18:53:46 +1000

    chore: Use rollup config from create-react-library

--- rollup.config.js
+++ rollup.config.js
@@ -1,101 +1,39 @@
-import pkg from './package.json';
-import path from 'path';
-import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
-import { uglify } from 'rollup-plugin-uglify';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
 import postcss from 'rollup-plugin-postcss';
 
-// eslint-disable-next-line
-const babelConfig = require('./babel.config');
+import pkg from './package.json';
 
-const getRollupConfig = ({ pkg, pwd, buildName }) => {
-  const SOURCE_DIR = path.resolve(pwd, 'src');
-  const DIST_DIR = path.resolve(pwd, 'dist');
-
-  const baseConfig = {
-    input: `${SOURCE_DIR}/index.js`,
-    plugins: [
-      babel({ exclude: '**/node_modules/**', ...babelConfig }),
-      resolve({ browser: true }),
-      commonjs(),
-      postcss()
-    ]
-  };
-
-  const esConfig = Object.assign({}, baseConfig, {
-    output: {
-      file: `${DIST_DIR}/${buildName}.es.js`,
-      format: 'esm'
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
     },
-    external: [
-      ...Object.keys(pkg.peerDependencies),
-      ...Object.keys(pkg.dependencies),
-      'react-transition-group/Transition'
-    ]
-  });
-
-  const cjsConfig = Object.assign({}, esConfig, {
-    output: {
-      file: `${DIST_DIR}/${buildName}.cjs.js`,
-      format: 'cjs'
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
     }
-  });
-
-  const globals = {
-    classnames: 'classNames',
-    polished: 'polished',
-    'prop-types': 'PropTypes',
-    'emotion-theming': 'emotionTheming',
-    emotion: 'emotion',
-    react: 'React',
-    'react-dom': 'ReactDom',
-    'react-emotion': 'reactEmotion',
-    'styled-components': 'styled'
-  };
-
-  const umdConfig = Object.assign({}, baseConfig, {
-    output: {
-      name: 'smoothUI',
-      file: `${DIST_DIR}/${buildName}.js`,
-      format: 'umd',
-      globals,
-      exports: 'named',
-      sourcemap: false
-    },
-    external: Object.keys(globals),
-    plugins: [...baseConfig.plugins]
-  });
-
-  const minConfig = Object.assign({}, umdConfig, {
-    output: {
-      ...umdConfig.output,
-      file: `${DIST_DIR}/${buildName}.js`
-    },
-    plugins: [
-      ...umdConfig.plugins,
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      uglify({
-        compress: {
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: true,
-          warnings: false
-        }
-      })
-    ]
-  });
-
-  if (process.env.WATCH_MODE) {
-    return [esConfig];
-  }
-
-  return [umdConfig, esConfig, cjsConfig];
+  ],
+  plugins: [
+    external(),
+    postcss({
+      modules: true
+    }),
+    url(),
+    svgr(),
+    babel({
+      exclude: 'node_modules/**',
+      plugins: []
+    }),
+    resolve(),
+    commonjs()
+  ]
 };
-
-export default getRollupConfig({
-  pwd: __dirname,
-  buildName: 'index',
-  pkg
-});
