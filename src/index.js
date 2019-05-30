565a58bcf1a2ec34c3f8c8da12340798eec9a082
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-05-30 18:59:45 +1000

    feat: Get maxWidth from props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 // Components
 export { Button } from './Button';
+export { Link, Anchor } from './Link';
 export { Grid, Cell } from './Grid';
 export { Container, Col, Row } from './FlexBox';
-export { FormGroup } from './Forms';
+export { Card, CardBody, CardHeader, CardFooter } from './Card';
+export { Heading } from './Typography/Heading';
+export { Spinner } from './Spinner/Spinner';
+export { RangeSlider } from './Slider/RangeSlider';
+export { Table, TBody, THead, Tr, Th, Td } from './Table';
+export { Badge } from './simpleComponents/Badge';
+export {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useToggle
+} from './Modal/Modal';
 
 // Presets
+export {
+  FormGroup,
+  FormInput,
+  FormSelect,
+  Label,
+  HelpText,
+  Feedback,
+  Input
+} from './Forms';
 export { Header } from './Header';
+
+// Misc
+export { Styled } from './Styled';
 
 // Core modules and optionals
 export { default as defaultTheme } from './Themes/stylestrap';
