2ca298cdc5d12ba982f24627d3ba660aa724d6de
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-06-09 23:43:51 +1000

    Add size props to Button

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,9 +1,22 @@
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles, getStylesByFlavor } from '../core/ruleEngine';
 
-const Button = withStyles(['Button', 'btn'], (props, theme) => {
-  return getStylesByFlavor({ color: 'primary', ...props }, theme, true);
-})('button');
+const Button = withStyles(
+  [
+    'Button',
+    props =>
+      classNames({
+        btn: true,
+        'btn-sm': props.size === 'sm',
+        'btn-lg': props.size === 'lg',
+        'btn-block': props.block
+      })
+  ],
+  (props, theme) => {
+    return getStylesByFlavor({ color: 'primary', ...props }, theme, true);
+  }
+)('button');
 
 Button.propTypes = {
   color: PropTypes.oneOf([
