b5c56c2a0461f2b74eb9a0f9796d2c71436449a2
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-09-14 16:26:10 +1000

    Update exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,19 +1,14 @@
-// Components
-export { Button } from './Button/Button';
-export { Link, Anchor } from './Link';
-export { Grid, Cell } from './Grid';
-export { Container, Col, Row } from './FlexBox';
-export { Card, CardBody, CardHeader, CardFooter } from './Card';
-export { Heading } from './Typography/Heading';
-export { Spinner } from './Spinner/Spinner';
-export { RangeSlider } from './Slider/RangeSlider';
-export { Table, TBody, THead, Tr, Th, Td } from './Table';
-export { Badge } from './simpleComponents/Badge';
-export { Modal, ModalHeader, ModalBody, ModalFooter, useToggle } from './Modal/Modal';
-
-// Presets
-export { FormGroup, FormInput, FormSelect, Label, HelpText, Feedback, Input } from './Forms';
-export { Header } from './Header';
-
-// Misc
-export { Styled } from './Styled';
+export * from './Button';
+export * from './Link';
+export * from './Grid';
+export * from './FlexBox';
+export * from './Card';
+export * from './Typography';
+export * from './Spinner';
+export * from './Slider';
+export * from './Table';
+export * from './Badge';
+export * from './Modal';
+export * from './Forms';
+export * from './Header';
+export * from './Styled';
