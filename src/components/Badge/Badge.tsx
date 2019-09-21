cdf2f98a1411adbc46351497063482d8218d63e4
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-09-21 10:43:58 +1000

    Add support for pill badges and link badges

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { makeComponent } from '../../core';
 
 type Badge = {
-  color: string;
+  color?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info' | 'light' | 'dark';
 };
 
 const Badge: React.FC<Badge> = makeComponent('Badge')
-  .classNames('badge')
+  .classNames(props => ({
+    badge: true,
+    'badge-pill': props.variant === 'pill',
+  }))
+  .defaultProps({
+    color: 'primary',
+  })
   .styles((props, theme) => ({
     backgroundColor: props.color,
-    color: theme.helpers.colorByLuminance(props.color, theme),
+    color: theme.colorByLuminance(props.color),
+  }))
+  .props(props => ({
+    as: props.href && 'a',
   }))
   .create('span');
 
