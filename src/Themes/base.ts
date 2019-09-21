c674c4a07865b02bb81b89d5a8e9554182710532
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-09-21 21:49:17 +1000

    Fix issues in the base theme

--- src/Themes/base.ts
+++ src/Themes/base.ts
@@ -1,14 +1,14 @@
-import { shade, getLuminance } from 'polished';
+import { shade, getLuminance, lighten } from 'polished';
 import state from '../core/ruleEngine/state';
 
 function getColorShade(colorEnum, type) {
   const color = state.theme.colors[colorEnum];
   const colorMap = {
-    '-2': shade(0.3, color), // TODO replace shade with some other helper to make them brighter
-    '-1': shade(0.2, color), // TODO replace shade with some other helper to make them brighter
+    '-2': lighten(0.15, color),
+    '-1': lighten(0.075, color),
     0: color,
-    1: shade(0.2, color),
-    2: shade(0.3, color),
+    1: shade(0.1, color),
+    2: shade(0.2, color),
   };
 
   return colorMap[type];
@@ -18,13 +18,8 @@ function colorByLuminance(color) {
   return getLuminance(state.theme.colors[color] || color) < 0.7 ? '#FFF' : '#666';
 }
 
-function borderColorByLuminance(color) {
-  return getLuminance(state.theme.colors[color] || color) < 0.7 ? '#AAA' : '#FFF';
-}
-
 const theme = {
   colorByLuminance,
-  borderColorByLuminance,
   getColorShade,
   colors: {},
   breakpoints: {},
