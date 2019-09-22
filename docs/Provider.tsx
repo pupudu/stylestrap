4225c11d75209ba820ad65b494f34390516e8276
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-09-22 16:43:04 +1000

    Improve the theme editor view

--- docs/Provider.tsx
+++ docs/Provider.tsx
@@ -1,40 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './';
-import '../src/Stylestrap';
+
+const defaultCssUrl = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
 
 const cacheState: {
   setState?: Function;
   state?: any;
+  setBaseCssUrl?: Function;
+  baseCssUrl?: any;
 } = {};
+
+export const getState = () => cacheState.state && cacheState.state;
+export const getBaseCssUrl = () => cacheState.baseCssUrl && cacheState.baseCssUrl;
 
 export function setState(theme) {
   if (cacheState.setState) {
-    cacheState.setState(theme);
-  } else {
-    throw new Error('Attempted to set cache state. But the state has not been initialized yet.');
+    return cacheState.setState(theme);
   }
+  throw new Error('Attempted to set cache state. But the state has not been initialized yet.');
 }
 
-export function getState() {
-  if (cacheState.state) {
-    return cacheState.state;
+export function setBaseCssUrl(theme = defaultCssUrl) {
+  if (cacheState.setBaseCssUrl) {
+    return cacheState.setBaseCssUrl(theme);
   }
-  throw new Error('Attempted to getState. But state is empty. Have you set the Theme Provider?');
+  throw new Error('Attempted to set cache state. But the state has not been initialized yet.');
 }
 
 export default function DoczRoot(props) {
   const [state, setState] = useState(defaultTheme);
+  const [baseCssUrl, setBaseCssUrl] = useState(defaultCssUrl);
+
   useEffect(() => {
     cacheState.state = state;
     cacheState.setState = setState;
-  }, [state]);
+    cacheState.baseCssUrl = baseCssUrl;
+    cacheState.setBaseCssUrl = setBaseCssUrl;
+  }, [baseCssUrl, state]);
+
   return (
     <div>
       <link
         href="https://fonts.googleapis.com/css?family=Barriecito&display=swap"
         rel="stylesheet"
       />
+      <link href={baseCssUrl} rel="stylesheet" />
       <ThemeProvider theme={state} {...props} />
     </div>
   );
--- docs/ThemeEditor.tsx
+++ docs/ThemeEditor.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
 import 'codemirror/theme/material.css';
 import 'codemirror/mode/javascript/javascript';
-import { Button, Card } from './';
-import { getState, setState } from './Provider';
-
-const currentTheme = getState();
-const { __filemeta, ...themeWithoutFileMeta } = currentTheme as any;
-const defaultTheme = JSON.stringify(themeWithoutFileMeta, null, 2).replace(
-  /\"([^(\")"]+)\":/g,
-  '$1:'
-);
-
-let text = defaultTheme;
+import { Button, Card, FormInput as Input, Styled } from './';
+import { getState, setState, setBaseCssUrl, getBaseCssUrl } from './Provider';
 
 export function Editor() {
+  const currentTheme = getState();
+  const { __filemeta, ...themeWithoutFileMeta } = currentTheme as any;
+  const defaultTheme = JSON.stringify(themeWithoutFileMeta, null, 2).replace(
+    /\"([^(\")"]+)\":/g,
+    '$1:'
+  );
+
+  // Not using state, as we don't want to re-render every time this changes
+  let defaultThemeText = defaultTheme;
+  let cssUrl = getBaseCssUrl();
+
   return (
     <Card css={{ padding: 'sm' }}>
       <CodeMirror
@@ -24,14 +26,25 @@ export function Editor() {
           theme: 'material',
           lineNumbers: true,
         }}
-        onChange={(_editor, _data, value) => (text = value)}
+        onChange={(_editor, _data, value) => (defaultThemeText = value)}
       />
+      <Styled marginTop="sm">
+        <Input
+          type="text"
+          label="Base Bootstrap CSS Url"
+          placeholder={cssUrl}
+          onChange={(e: any) => {
+            cssUrl = e.target.value;
+          }}
+        />
+      </Styled>
       <br />
       <div>
         <Button
           onClick={() => {
-            const newTheme = Function(`return ${text}`)();
+            const newTheme = Function(`return ${defaultThemeText}`)();
             setState({ ...currentTheme, ...newTheme });
+            setBaseCssUrl(cssUrl);
           }}
         >
           Apply Theme
