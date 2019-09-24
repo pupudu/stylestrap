0575677a4377a951042e7dc1255cbec6619c662f
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-09-24 17:13:44 +1000

    Fix bugs in the Grid components

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -6,14 +6,18 @@ const Grid = makeComponent('Grid')
       columns => (isNaN(+columns) ? columns : `repeat(${+columns}, 1fr)`),
       props.columns
     );
+    const gridTemplateRows = transform(
+      rows => (isNaN(+rows) ? rows : `repeat(${+rows}, 1fr)`),
+      props.columns
+    );
     return {
       display: 'grid',
       position: 'relative',
       gridTemplateColumns,
+      gridTemplateRows,
       padding: props.padding,
       margin: props.margin,
       gridGap: props.gap,
-      gridTemplateRows: props.rows,
     };
   })
   .create();
@@ -21,12 +25,12 @@ const Grid = makeComponent('Grid')
 const Cell = makeComponent('Cell')
   .styles(({ align: alignSelf, position, ...props }) => {
     const [gridRowStart, gridRowEnd] = transform(
-      (row = []) => (typeof row === 'number' ? [row, row + 1] : row),
-      props.row
+      (row = []) => (typeof row !== 'number' ? row : row > 0 ? [row, row + 1] : [row, row - 1]),
+      Array.isArray(props.row) ? props.row : Number(props.row) || undefined
     );
     const [gridColumnStart, gridColumnEnd] = transform(
-      (col = []) => (typeof col === 'number' ? [col, col + 1] : col),
-      props.col
+      (col = []) => (typeof col !== 'number' ? col : col > 0 ? [col, col + 1] : [col, col - 1]),
+      Array.isArray(props.col) ? props.col : Number(props.col) || undefined
     );
     return { alignSelf, position, gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd };
   })
