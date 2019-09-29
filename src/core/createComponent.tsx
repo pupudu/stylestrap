d7c45b84a6f88378c989b4950913f2f5d2baa627
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-09-29 11:08:52 +1000

    Make sure the as prop is consumed by the last component on the chain

--- src/core/createComponent.tsx
+++ src/core/createComponent.tsx
@@ -43,15 +43,12 @@ class Builder<T> {
     return this;
   }
 
-  create(component: any = 'div'): React.FC<T> {
-    const {
-      name,
-      transformedProps,
-      defaultProps,
-      initialStyles,
-      initialClassNames,
-      raw,
-    } = this.state;
+  /**
+   * Create the Styled Component to apply stylestrap styles
+   * @private
+   */
+  __getStyledComponent__(Component) {
+    const { name, initialStyles, raw } = this.state;
 
     const styleMapper = props => ({
       ...callOrReturn(initialStyles, props, props.theme),
@@ -60,24 +57,57 @@ class Builder<T> {
 
     const getRawStyles = props => callOrReturn(raw, props, props.theme);
 
-    const StyledComponent = (styled[component] || styled(component))`
+    return (styled[Component] || styled(Component))`
       &&& {
         ${propsWithTheme => getStyleString(propsWithTheme, styleMapper, name)};
         ${propsWithTheme => getRawStyles(propsWithTheme)}
       }
     `;
+  }
 
-    const Wrapped: React.FC = originalProps => {
-      const mergedProps = {
-        ...defaultProps,
+  create(Component: any = 'div'): React.FC<T> {
+    const { name, transformedProps, defaultProps, initialClassNames } = this.state;
+
+    const StyledComponent = this.__getStyledComponent__(Component);
+
+    const Wrapped: React.FC = (originalProps: any) => {
+      /**
+       * as prop should be passed to `transformedProps` and `initialClassNames` calls.
+       * So we create a props object with the as prop,
+       * either from the original as prop, or the renamed __as__ prop.
+       */
+      const propsWithAs = {
         ...originalProps,
-        ...callOrReturn(transformedProps, originalProps),
+        as: originalProps.as || originalProps.__as__,
       };
 
+      /**
+       * Merge default props, original props(with as) and transformed props
+       */
+      const mergedProps = {
+        ...defaultProps,
+        ...propsWithAs,
+        ...callOrReturn(transformedProps, propsWithAs),
+      };
+
+      /**
+       * Generate classnames based on merged props
+       */
       const className = classnames(
         callOrReturn(initialClassNames, mergedProps),
         mergedProps.className
       );
+
+      /**
+       * If the component is not a string, we delete the as prop
+       * Otherwise, the HoC component will be replaced.
+       * By renaming it to __as__, we can instead pass the as prop down,
+       * to be consumed by the last non HoC component
+       */
+      if (typeof Component !== 'string') {
+        mergedProps.__as__ = mergedProps.as;
+        delete mergedProps.as;
+      }
 
       return <StyledComponent {...mergedProps} className={className} />;
     };
