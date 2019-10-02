ee10e3e9d589488005588b15853a21060dfec701
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-10-02 21:23:18 +1000

    Add function to filter empty keys from objects

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,4 @@
 export * from './ruleEngine';
 export * from './createComponent';
 export * from './Types';
+export * from './utils';
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -8,3 +8,23 @@ export function callOrReturn(target, ...args) {
 export function toKebabCase(str) {
   return str.replace(/([A-Z])/g, Cap => `-${Cap.toLowerCase()}`);
 }
+
+/**
+ * Filter out entries which have ['false', 'null', 'undefined', ''] as keys
+ * or undefined as the value (using the option)
+ *
+ * Note: other empty values like null, 0, '', and false are allowed as properties
+ */
+export function filterEmptyKeys<T>(obj: T): T {
+  return Object.entries(obj)
+    .filter(
+      ([key, value]) => value !== undefined && !['false', 'null', 'undefined', ''].includes(key)
+    )
+    .reduce(
+      (carry, [key, value]) => {
+        carry[key] = value;
+        return carry;
+      },
+      {} as T
+    );
+}
