cc8137d7d20e16240bb56fa29c7e3bd10fc15d7c
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-10-02 21:28:10 +1000

    Complete the card docs except for the layout section

--- src/components/Card/__docs__/Presets.tsx
+++ src/components/Card/__docs__/Presets.tsx
@@ -15,6 +15,8 @@ import {
   BlockquoteFooter,
   ListGroup,
   ListGroupItem,
+  CardImgOverlay,
+  BgImg,
 } from '../../../';
 import src from './cardImg.jpg';
 
@@ -34,15 +36,126 @@ export function BasicCard() {
   );
 }
 
+export function CardImageTop() {
+  return (
+    <Card width="15rem">
+      <CardImg src={src} />
+      <CardBody>
+        <CardText>Some text description of the image below the image</CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+export function CardImageOverlay() {
+  return (
+    <Card width="20rem" textColor="#FFF">
+      <CardImg src={src} />
+      <CardImgOverlay>
+        <CardTitle>Card title</CardTitle>
+        <CardText>
+          This is a card with supporting text below as a natural lead-in to additional content. This
+          content is a little bit longer.
+        </CardText>
+        <CardText>Last updated 3 minutes ago</CardText>
+      </CardImgOverlay>
+    </Card>
+  );
+}
+
+export function CardImageHorizontal() {
+  return (
+    <Card width="40rem">
+      <Grid columns="2">
+        <CardImg src={src} />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardText>
+            This is a card with supporting text below as a natural lead-in to additional content.
+            This content is a little bit longer.
+          </CardText>
+          <CardText muted>Last updated 3 minutes ago</CardText>
+        </CardBody>
+      </Grid>
+    </Card>
+  );
+}
+
+export function CardBgImg() {
+  return (
+    <Grid columns="2" gap="sm">
+      <div>
+        <Card width="25rem">
+          <Grid columns="2">
+            <BgImg src={src} />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardText>
+                Image fits perfectly on the space available, by cropping the image horizontally.
+              </CardText>
+              <CardText muted>Last updated 3 minutes ago</CardText>
+            </CardBody>
+          </Grid>
+        </Card>
+      </div>
+      <div>
+        <Card width="25rem">
+          <Grid columns="2">
+            <CardImg src={src} />
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardText>
+                Image is not cropped and some white space is added on bottom to match the content
+                height.
+              </CardText>
+              <CardText muted>Last updated 3 minutes ago</CardText>
+            </CardBody>
+          </Grid>
+        </Card>
+      </div>
+    </Grid>
+  );
+}
+
+export function CardBgImgOverlay() {
+  return (
+    <Grid columns="18rem 18rem" gap="sm">
+      <div>
+        <Card width="18rem" textColor="white">
+          <BgImg src={src}>
+            <CardBody>
+              <CardTitle>Card title</CardTitle>
+              <CardText>
+                This content is a little bit longer. This image is cropped to fit both the content
+                and image without any unintended white spaces
+              </CardText>
+              <CardText>Last updated 3 minutes ago</CardText>
+            </CardBody>
+          </BgImg>
+        </Card>
+      </div>
+      <div>
+        <Card width="18rem" textColor="#FFF">
+          <CardImg src={src} />
+          <CardImgOverlay>
+            <CardTitle>Card title</CardTitle>
+            <CardText>
+              This content is a little bit longer. The image is not cropped in this case. Therefore
+              if the content is too long, some of the content will be cropped from the bottom side
+              of the card.
+            </CardText>
+            <CardText>Last updated 3 minutes ago</CardText>
+          </CardImgOverlay>
+        </Card>
+      </div>
+    </Grid>
+  );
+}
+
 export function CardImagePreview() {
   return (
     <Grid columns="3">
-      <Card width="15rem">
-        <CardImg src={src} />
-        <CardBody>
-          <CardText>Some text description of the image below the image</CardText>
-        </CardBody>
-      </Card>
+      <CardImageTop />
       <Card width="15rem">
         <CardBody>
           <CardText>Some text description of the image above the image</CardText>
@@ -181,5 +294,64 @@ export function BlockquoteCard() {
         </Blockquote>
       </CardBody>
     </Card>
+  );
+}
+
+export function PlainCard(props) {
+  return (
+    <Card {...props}>
+      <CardHeader>Heading</CardHeader>
+      <CardBody>
+        <CardTitle>Card Title</CardTitle>
+        <CardText>
+          Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+export function HeaderLessPlainCard(props) {
+  return (
+    <Card {...props}>
+      <CardBody>
+        <CardTitle>Card Title</CardTitle>
+        <CardText>
+          Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+}
+
+export function CardStylePlain() {
+  return (
+    <Grid columns="2" gap="md">
+      <PlainCard color="primary" />
+      <PlainCard color="secondary" />
+      <PlainCard color="success" />
+      <PlainCard color="warning" />
+      <PlainCard color="danger" />
+      <PlainCard color="info" />
+      <PlainCard color="light" />
+      <PlainCard color="dark" />
+    </Grid>
+  );
+}
+
+export function CardStyleAccent() {
+  return (
+    <Grid columns="2" gap="md">
+      <HeaderLessPlainCard color="primary" flavor="accent" />
+      <HeaderLessPlainCard color="secondary" flavor="accent" />
+      <HeaderLessPlainCard color="success" flavor="accent" />
+      <HeaderLessPlainCard color="warning" flavor="accent" />
+      <HeaderLessPlainCard color="danger" flavor="accent" />
+      <HeaderLessPlainCard color="info" flavor="accent" />
+      <HeaderLessPlainCard color="light" flavor="accent" />
+      <HeaderLessPlainCard color="dark" flavor="accent" />
+    </Grid>
   );
 }
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,2 +1,4 @@
 export * from './Card';
 export * from './CardImg';
+export * from './CardHeaderNav';
+export * from './CardImgOverlay';
