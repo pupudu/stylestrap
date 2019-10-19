3cd12464d2701c31f68cb94c26dfa73d1e1d65f0
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-10-19 21:15:14 +1100

    Add support for colors and forward props to base table components

--- src/components/Table/TableComponents.tsx
+++ src/components/Table/TableComponents.tsx
@@ -1,7 +1,29 @@
 import { makeComponent } from '../../core';
 
-const Tr = makeComponent('Tr').create('tr');
-const Th = makeComponent('Th').create('th');
-const Td = makeComponent('Td').create('td');
+const Tr = makeComponent('Tr')
+  .forwardProps(props => ({
+    color: props.color,
+  }))
+  .styles((props, theme) => ({
+    backgroundColor: props.color,
+    color: theme.colorByLuminance(props.color),
+  }))
+  .create('tr');
+
+const Th = makeComponent('Th')
+  .styles((props, theme) => ({
+    borderColor: theme.shadeByLuminance(props.color),
+    backgroundColor: props.color,
+    color: theme.colorByLuminance(props.color),
+  }))
+  .create('th');
+
+const Td = makeComponent('Td')
+  .styles((props, theme) => ({
+    borderColor: theme.shadeByLuminance(props.color),
+    backgroundColor: props.color,
+    color: theme.colorByLuminance(props.color),
+  }))
+  .create('td');
 
 export { Tr, Th, Td };
