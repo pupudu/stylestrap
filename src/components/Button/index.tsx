93299e56c72ce1446fd3434858d8e978cfdc009f
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-10-21 22:13:28 +1100

    Export types

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,1 +1,2 @@
 export * from './Button';
+export { ButtonProps } from './Types';
--- src/components/ButtonGroup/index.ts
+++ src/components/ButtonGroup/index.ts
@@ -1,2 +1,3 @@
 export * from './ButtonGroup';
 export * from './ButtonToolbar';
+export { ButtonGroupProps, ButtonToolbarProps } from './Types';
