ddbdbc638f460f84da95097ccd75e957dbe1f8d1
Pubudu Dodangoda <pubududodangoda@hipagesgroup.com.au>
2019-10-23 17:04:44 +1100

    Move the Grid Presets to Grid directory

--- src/components/Grid/Presets.tsx
+++ src/components/Grid/Presets.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Grid, Cell, Styled, Box, FlexBox } from '../';
-import { ColoredDiv } from './ColoredDiv';
+import { Grid, Cell, Styled, Box, FlexBox } from '../../';
+import { ColoredDiv } from '../../docs/Presets';
+import { useColorMode } from 'theme-ui';
 
 const areas = `
   "HEAD HEAD"
@@ -11,7 +12,7 @@ const areas = `
 const commonProps = {
   as: ColoredDiv,
   height: 'initial',
-  css: {
+  $css: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
@@ -44,37 +45,40 @@ const spacedCss = {
   border: '1px dashed black',
   fontSize: '16px',
   height: '150px',
-  background: '#f5f5f5',
 };
 
-const SpacedDiv = props => (
-  <Box
-    {...props}
-    css={{
-      ...spacedCss,
-      ...props.css,
-      borderLeftWidth: props.skip ? 0 : '3px',
-      borderLeftColor: 'info',
-      borderRightWidth: props.skip ? 0 : '3px',
-      borderRightColor: 'info',
-      borderTopWidth: props.skipY ? 0 : '3px',
-      borderTopColor: 'danger',
-      borderBottomWidth: props.skipY ? 0 : '3px',
-      borderBottomColor: 'danger',
-    }}
-  />
-);
+const SpacedDiv = props => {
+  const [mode] = useColorMode();
+  return (
+    <Box
+      {...props}
+      $css={{
+        ...spacedCss,
+        ...props.$css,
+        borderLeftWidth: props.skip ? 0 : '3px',
+        borderLeftColor: 'info',
+        borderRightWidth: props.skip ? 0 : '3px',
+        borderRightColor: 'info',
+        borderTopWidth: props.skipY ? 0 : '3px',
+        borderTopColor: 'danger',
+        borderBottomWidth: props.skipY ? 0 : '3px',
+        borderBottomColor: 'danger',
+        color: mode === 'dark' ? '#FFF' : '#000',
+      }}
+    />
+  );
+};
 
-const BigSpan = props => <Styled {...props} css={{ ...props.css }} as="span" />;
+const BigSpan = props => <Styled {...props} $css={{ ...props.$css }} as="span" />;
 
 export function GridLines() {
   return (
     <div>
       <Grid columns="3">
         <SpacedDiv />
-        <SpacedDiv skip css={{ justifyContent: 'center' }}>
+        <SpacedDiv skip $css={{ justifyContent: 'center' }}>
           <FlexBox height="100%" justifyContent="space-between" direction="column">
-            <BigSpan css={{ textAlign: 'center' }}>
+            <BigSpan $css={{ textAlign: 'center' }}>
               <div>⇧</div>
               <div>1 (or -4)</div>
             </BigSpan>
@@ -85,16 +89,16 @@ export function GridLines() {
       <Grid columns="3">
         <SpacedDiv skipY>
           <BigSpan>⇦1 (or -4)</BigSpan>
-          <BigSpan></BigSpan>
+          <BigSpan />
         </SpacedDiv>
         <SpacedDiv skip skipY>
           <BigSpan>⇦2 (or -3)</BigSpan>
           <FlexBox height="100%" justifyContent="space-between" direction="column">
-            <BigSpan css={{ textAlign: 'center' }}>
+            <BigSpan $css={{ textAlign: 'center' }}>
               <div>⇧</div>
               <div>2 (or -3)</div>
             </BigSpan>
-            <BigSpan css={{ textAlign: 'center' }}>
+            <BigSpan $css={{ textAlign: 'center' }}>
               <div>3 (or -2)</div>
               <div>⇩</div>
             </BigSpan>
@@ -102,15 +106,15 @@ export function GridLines() {
           <BigSpan>3 (or -2)⇨</BigSpan>
         </SpacedDiv>
         <SpacedDiv skipY>
-          <BigSpan></BigSpan>
+          <BigSpan />
           <BigSpan>4 (or -1)⇨</BigSpan>
         </SpacedDiv>
       </Grid>
       <Grid columns="3">
         <SpacedDiv />
-        <SpacedDiv skip css={{ justifyContent: 'center' }}>
+        <SpacedDiv skip $css={{ justifyContent: 'center' }}>
           <FlexBox height="100%" justifyContent="flex-end" direction="column">
-            <BigSpan css={{ textAlign: 'center' }}>
+            <BigSpan $css={{ textAlign: 'center' }}>
               <div>4 (or -1)</div>
               <div>⇩</div>
             </BigSpan>
@@ -133,14 +137,14 @@ export function HorizontalGridLines() {
       <Grid columns="3">
         <SpacedDiv>
           <BigSpan>⇦1 (or -4)</BigSpan>
-          <BigSpan></BigSpan>
+          <BigSpan />
         </SpacedDiv>
         <SpacedDiv skip>
           <BigSpan>⇦2 (or -3)</BigSpan>
           <BigSpan>3 (or -2)⇨</BigSpan>
         </SpacedDiv>
         <SpacedDiv>
-          <BigSpan></BigSpan>
+          <BigSpan />
           <BigSpan>4 (or -1)⇨</BigSpan>
         </SpacedDiv>
       </Grid>
@@ -154,12 +158,12 @@ export function HorizontalGridLines() {
 }
 
 const Word = ({ color, ...props }: any) => {
-  return <Box {...props} css={{ background: color }} as="span" display="inline" />;
+  return <Box {...props} $css={{ background: color }} as="span" display="inline" />;
 };
 
 export const WordTemplate = () => {
   return (
-    <Grid css={{ fontFamily: 'monospace', fontSize: '16px' }}>
+    <Grid $css={{ fontFamily: 'monospace', fontSize: '16px' }}>
       <div>
         "<Word color="#c7e2ff">HEAD</Word> <Word color="#c7e2ff">HEAD</Word>"
       </div>
