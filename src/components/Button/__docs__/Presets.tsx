d4fdb77f9e752354c404fb812fd6d5a4cf424f10
Pubudu Dodangoda <pubudu.dodan@gmail.com>
2020-08-11 12:21:59 +1000

    Update Presets.tsx

--- src/components/Button/__docs__/Presets.tsx
+++ src/components/Button/__docs__/Presets.tsx
@@ -15,5 +15,19 @@ const LoadingButtonBase = props => {
   );
 };
 
+export const FigmaEmbed = () => {
+  return (
+    <div>
+      <iframe
+        title="blah"
+        height="450"
+        width="800"
+        src="https://www.figma.com/embed?embed_host=astra&url=https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File"
+        allowfullscreen
+      />
+    </div>
+  );
+};
+
 export const Presets = {} as any;
 Presets.LoadingButton = LoadingButtonBase;
